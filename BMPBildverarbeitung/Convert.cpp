58e789377687a5b60ba3ba44c1f0376e8bb0e952
Vuizur <Vuizur@users.noreply.github.com>
2017-08-20 20:21:30 +0200

    Comment out sample function

--- BMPBildverarbeitung/Convert.cpp
+++ BMPBildverarbeitung/Convert.cpp
@@ -12,15 +12,15 @@ System::Drawing::Bitmap ^ Convert::ToBitmap(BMP bmp)
 
 BMP Convert::ToBMP(System::Drawing::Bitmap ^ bitmap)
 {
-	Rectangle rect = Rectangle(0, 0, bitmap->Width, bitmap->Height);
-	BitmapData^ bmpData = bitmap->LockBits(rect, ImageLockMode::ReadOnly, bitmap->PixelFormat);
-	System::IntPtr ptr = bmpData->Scan0;
-	int bytes = System::Math::Abs(bmpData->Stride) * bitmap->Height;
-	array<System::Byte>^ rgbValues = gcnew array<System::Byte>(bytes);
-	Marshal::Copy(ptr, rgbValues, 0, bytes);
-	bitmap->UnlockBits(bmpData);
-	GCHandle handle = GCHandle::Alloc(rgbValues, GCHandleType::Pinned);
-	unsigned char* data = (unsigned char*)(void*)handle.AddrOfPinnedObject;	
+	//Rectangle rect = Rectangle(0, 0, bitmap->Width, bitmap->Height);
+	//BitmapData^ bmpData = bitmap->LockBits(rect, ImageLockMode::ReadOnly, bitmap->PixelFormat);
+	//System::IntPtr ptr = bmpData->Scan0;
+	//int bytes = System::Math::Abs(bmpData->Stride) * bitmap->Height;
+	//array<System::Byte>^ rgbValues = gcnew array<System::Byte>(bytes);
+	//Marshal::Copy(ptr, rgbValues, 0, bytes);
+	//bitmap->UnlockBits(bmpData);
+	//GCHandle^ handle = GCHandle::Alloc(rgbValues, GCHandleType::Pinned);
+	//unsigned char* data = (unsigned char*)(void*)handle->AddrOfPinnedObject;	
 	//??
 
 	return BMP();
