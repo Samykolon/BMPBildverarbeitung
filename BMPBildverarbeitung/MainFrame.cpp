7ca099c9c03e97258ccd422ef9cb186f3ba128cf
Vuizur <Vuizur@users.noreply.github.com>
2017-10-09 14:00:52 +0200

    Update Backgroundworkers

--- BMPBildverarbeitung/MainFrame.cpp
+++ BMPBildverarbeitung/MainFrame.cpp
@@ -25,38 +25,60 @@ void Main(array<String^>^ args)
 	Application::Run(%form);
 }
 
-//inline void MainFrame::bwHSV_DoWork(System::Object ^ sender, System::ComponentModel::DoWorkEventArgs ^ e)
-//{
-//	IsProcessing = true;
-//	msclr::interop::marshal_context context;
-//	Filters::ApplySobel(context.marshal_as<const char*>(FilePath));
-//}
-//
-//inline void MainFrame::bwHSV_RunWorkerCompleted(Object^ sender, RunWorkerCompletedEventArgs^ e) {
-//	FilePath = "Test.bmp";
-//	UpdatePicture();
-//	IsProcessing = false;
-//}
-//
-//void BMPBildverarbeitung::MainFrame::bwSobel_DoWork(System::Object ^ sender, System::ComponentModel::DoWorkEventArgs ^ e)
-//{
-//	throw gcnew System::NotImplementedException();
-//}
-//
-//void BMPBildverarbeitung::MainFrame::bwSobel_RunWorkerCompleted(Object ^ sender, RunWorkerCompletedEventArgs ^ e)
-//{
-//	throw gcnew System::NotImplementedException();
-//}
-//
-//void BMPBildverarbeitung::MainFrame::bwGauss_DoWork(System::Object ^ sender, System::ComponentModel::DoWorkEventArgs ^ e)
-//{
-//	throw gcnew System::NotImplementedException();
-//}
-//
-//void BMPBildverarbeitung::MainFrame::bwGauss_RunWorkerCompleted(Object ^ sender, RunWorkerCompletedEventArgs ^ e)
-//{
-//	throw gcnew System::NotImplementedException();
-//}
+inline void MainFrame::bwHSV_DoWork(System::Object ^ sender, System::ComponentModel::DoWorkEventArgs ^ e)
+{
+	IsProcessing = true;
+	double value = THelligkeit->Value;
+	double s = TSaturation->Value;
+	if (value < 20) {
+		value /= 20;
+	}
+	else if (value > 20) {
+		value = 1 + (value / 20);
+	}
+	else {
+		value = 1;
+	}
+	if (s < 20) {
+		s /= 20;
+	}
+	else if (s > 20) {
+		s = 1 + (s / 20);
+	}
+	else {
+		s = 1;
+	}
+	Filters::ChangeHSVValue(*BMPimage, 1, value, s);	
+}
+
+inline void MainFrame::bwHSV_RunWorkerCompleted(Object^ sender, RunWorkerCompletedEventArgs^ e) {
+	UpdatePicture();
+	IsProcessing = false;
+}
+
+void BMPBildverarbeitung::MainFrame::bwSobel_DoWork(System::Object ^ sender, System::ComponentModel::DoWorkEventArgs ^ e)
+{
+	
+	Filters::ApplySobel(*BMPimage);
+}
+
+void BMPBildverarbeitung::MainFrame::bwSobel_RunWorkerCompleted(Object ^ sender, RunWorkerCompletedEventArgs ^ e)
+{
+	UpdatePicture();
+	IsProcessing = false;
+}
+
+void BMPBildverarbeitung::MainFrame::bwGauss_DoWork(System::Object ^ sender, System::ComponentModel::DoWorkEventArgs ^ e)
+{
+
+	Filters::ApplyGaussFilterRGB(*BMPimage);
+}
+
+void BMPBildverarbeitung::MainFrame::bwGauss_RunWorkerCompleted(Object ^ sender, RunWorkerCompletedEventArgs ^ e)
+{
+	UpdatePicture();
+	IsProcessing = false;
+}
 
 inline System::Void BMPBildverarbeitung::MainFrame::beendenToolStripMenuItem_Click(System::Object ^ sender, System::EventArgs ^ e) {
 
@@ -66,27 +88,33 @@ inline System::Void BMPBildverarbeitung::MainFrame::beendenToolStripMenuItem_Cli
 
 inline System::Void BMPBildverarbeitung::MainFrame::BSobel_Click(System::Object ^ sender, System::EventArgs ^ e) {
 	
-	/*if (!IsProcessing) {
-		BackgroundWorker^ bw = gcnew BackgroundWorker();
-		bw->DoWork += gcnew DoWorkEventHandler(this, &MainFrame::bwBrightness_DoWork);
-		bw->RunWorkerCompleted += gcnew System::ComponentModel::RunWorkerCompletedEventHandler(this, &BMPBildverarbeitung::MainFrame::bwBrightness_RunWorkerCompleted);
-		bw->RunWorkerAsync();
+	if (!IsProcessing) {
 		IsProcessing = true;
-	}*/
-	Filters::ApplySobel(*BMPimage);
-	UpdatePicture();
-	groupBox1->Text = "Sobel-Filter";
-	BApply->Enabled = true;
+		BackgroundWorker^ bw = gcnew BackgroundWorker();
+		bw->DoWork += gcnew DoWorkEventHandler(this, &MainFrame::bwSobel_DoWork);
+		bw->RunWorkerCompleted += gcnew System::ComponentModel::RunWorkerCompletedEventHandler(this, &BMPBildverarbeitung::MainFrame::bwSobel_RunWorkerCompleted);
+		bw->RunWorkerAsync();	
+		groupBox1->Text = "Sobel-Filter";
+		BApply->Enabled = true;
+	}	
+	
 		
 }
 
 inline System::Void BMPBildverarbeitung::MainFrame::BGauss_Click(System::Object ^ sender, System::EventArgs ^ e) {
 
-	groupBox1->Text = "Gauß-Filter";
-	BApply->Enabled = true;
-	msclr::interop::marshal_context context;
-	Filters::ApplyGaussFilterRGB(*BMPimage);
-	pictureBox1->Image = ConvertBitmap::ToBitmap(BMPimage);
+	if (!IsProcessing) {
+		IsProcessing = true;
+		BackgroundWorker^ bw = gcnew BackgroundWorker();
+		bw->DoWork += gcnew DoWorkEventHandler(this, &MainFrame::bwGauss_DoWork);
+		bw->RunWorkerCompleted += gcnew System::ComponentModel::RunWorkerCompletedEventHandler(this, &BMPBildverarbeitung::MainFrame::bwGauss_RunWorkerCompleted);
+		bw->RunWorkerAsync();
+		groupBox1->Text = "Gauß-Filter";
+		BApply->Enabled = true;
+	}
+
+	
+	
 }
 
 inline System::Void BMPBildverarbeitung::MainFrame::BHelligkeit_Click(System::Object ^ sender, System::EventArgs ^ e) {
@@ -115,6 +143,8 @@ inline System::Void BMPBildverarbeitung::MainFrame::BSaettigung_Click(System::Ob
 
 	groupBox1->Text = "Sättigung";
 	BApply->Enabled = true;
+	Filters::TurnToGrayScaleOptimized(*BMPimage);
+	UpdatePicture();
 	//Filters::ApplyGaussFilterBW("BestesBild.bmp");
 }
 
