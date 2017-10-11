b34b5d1c0da9abb48c6d18afa50b61e5b39e1895
nikoslos <nikoslos@users.noreply.github.com>
2017-10-11 12:43:07 +0200

    Scale with SIMD solution

--- BMPBildverarbeitung/ScaleWithNN.cpp
+++ BMPBildverarbeitung/ScaleWithNN.cpp
@@ -25,3 +25,109 @@ void Filters::ScaleWithNN(BMP& image, int newHeight, int newWidth)
 	new(&image) BMP(out);
 
 }
+
+// Optimized Scale function
+void Filters::ScaleWithNNSIMD(BMP &image, int newHeight, int newWidth)
+{
+	BMP out;
+	out.SetSize(newWidth, newHeight);
+
+	int newWidth4 = (newWidth / 4) * 4 - 4;
+	
+	int newHeight4 = (newHeight / 4) * 4 - 4;
+	
+	
+	// Comment to enable SIMD
+	//newWidth4 = 0;
+	//newHeight4 = 0;
+
+	double xMult = (double)image.TellWidth() / newWidth;
+	double yMult = (double)image.TellHeight() / newHeight;
+
+	// Multiplier vector
+	__m128 xMultiplier = _mm_set1_ps(xMult);
+	__m128 yMultiplier = _mm_set1_ps(yMult);
+
+	__m128 counter = _mm_setr_ps(0, 1, 2, 3);
+	__m128 halfAdder = _mm_set1_ps(0.5);
+
+	// Buffer Contains for every position in the new image the position in the old image 
+	_int32 *bufferX = new _int32[newWidth];
+	_int32 *bufferY = new _int32[newHeight];
+
+	// Generate Lookup for X direction with SIMD instructions
+	for (int x = 0; x < newWidth4; x += 4)
+	{
+		// 128bit vector with 4 * 32bit x variables initilized
+		__m128 coordsX = _mm_set1_ps(x);
+
+		// Add iteration + 0..3
+		coordsX = _mm_add_ps(coordsX, counter);
+
+		// position * multiplier
+		coordsX = _mm_mul_ps(coordsX, xMultiplier);
+
+		// position + 0.5 and converted to integer vector
+		__m128i coordsXi = _mm_cvtps_epi32(_mm_add_ps(coordsX, halfAdder));
+
+		// Store in buffer
+		_mm_storeu_si128((__m128i*)(&bufferX[x]), coordsXi);
+	}
+
+	// Iterate through the rest of x
+	for (int x = newWidth4; x < newWidth; x++)
+	{
+		bufferX[x] = x * xMult + 0.5;
+	}
+
+	// Generate Lookup for Y direction with SIMD instructions
+	for (int y = 0; y < newHeight4; y += 4)
+	{
+		// 128bit vector with 4 * 32bit x variables initilized
+		__m128 coordsY = _mm_set1_ps(y);
+
+		// Add iteration + 0..3
+		coordsY = _mm_add_ps(coordsY, counter);
+
+		// position * multiplier
+		coordsY = _mm_mul_ps(coordsY, yMultiplier);
+
+		// position + 0.5 and converted to integer vector
+		__m128i coordsYi = _mm_cvtps_epi32(_mm_add_ps(coordsY, halfAdder));
+
+		// Store in buffer
+		_mm_storeu_si128((__m128i*)(&bufferY[y]), coordsYi);
+	}
+
+	// Iterate through the rest of y
+	for (int y = newHeight4; y < newHeight; y++)
+	{
+		bufferY[y] = y * yMult + 0.5;
+	}
+
+	for (int y = 0; y < newHeight; y++)
+	{
+		/*
+		int Y = bufferY[y];
+		for (int x = 0; x < newWidth4; x += 4)
+		{
+		__m128i pixels = _mm_set_epi32(*(__int32*)&image.Pixels[bufferX[x+0]][Y],
+		*(__int32*)&image.Pixels[bufferX[x+1]][Y],
+		*(__int32*)&image.Pixels[bufferX[x+2]][Y],
+		*(__int32*)&image.Pixels[bufferX[x+3]][Y]);
+
+		__m128i* store = (__m128i*)(&(out.Pixels[x][y]));
+		_mm_storeu_si128(store, pixels);
+		}
+		*/
+		for (int x = 0; x < newWidth; x++)
+		{
+			out.Pixels[x][y] = image.Pixels[bufferX[x]][bufferY[y]];
+		}
+	}
+	delete bufferX;
+	delete bufferY;
+
+	image.~BMP();
+	new(&image) BMP(out);
+}
